8d8eec8957fefc15e772d19e2f0d13e9fd0ede67
Daniel Meszaros <easimer@gmail.com>
2020-04-05 22:18:26 +0200

    Add cylindergen project

---
+++ cylindergen/main.cpp
@@ -0,0 +1,8 @@
+// === Copyright (c) 2020-2021 easimer.net. All rights reserved. ===
+//
+// Purpose: Generate 3D mesh from a Lindenmayer-system
+//
+
+int main(int argc, char** argv) {
+    return 0;
+}
