80db2270e6ea093fd5e9ba88fcb8d6d81d90d753
Daniel Meszaros <easimer@gmail.com>
2020-08-03 21:20:15 +0200

    Begin implementing anchor points

--- softbody/s_ext_plant.cpp
+++ softbody/s_ext_plant.cpp
@@ -7,9 +7,26 @@
 #include "common.h"
 #include "softbody.h"
 #include "s_ext.h"
+#include "m_sdf.h"
 
 class Plant_Simulation : public ISimulation_Extension {
     void post_prediction(IParticle_Manager* pman, System_State& s) override {
+        auto const anchor_point_min_dist = 2.0f;
+        auto const attachment_strength = 0.25f;
+        for (index_t i = 0; i < s.position.size(); i++) {
+            Vector<Vec3> anchor_points;
+
+            auto p = s.predicted_position[i];
+            for (auto& C : s.colliders_sdf) {
+                auto t = C.fun(p);
+                if (t < anchor_point_min_dist) {
+                    auto normal = sdf::normal(C.fun, p);
+                    auto anchor_point = p - t * normal;
+
+                    s.predicted_position[i] += attachment_strength * (anchor_point - s.predicted_position[i]);
+                }
+            }
+        }
     }
 };
 
