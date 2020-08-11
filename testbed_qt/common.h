8d4320fe79af8fe0920460b22e4c8b66ca550c26
Daniel Meszaros <easimer@gmail.com>
2020-08-11 20:24:26 +0200

    testbed_qt: precompile Qt headers

--- testbed_qt/common.h
+++ testbed_qt/common.h
@@ -5,10 +5,39 @@
 
 #pragma once
 
-#if __cplusplus
+#define GLM_ENABLE_EXPERIMENTAL
+
+#include <cassert>
+#include <cstdio>
+
 #include <memory>
 #include <functional>
 #include <optional>
+#include <vector>
+#include <unordered_map>
+#include <list>
+
+#include <glm/gtc/constants.hpp>
+#include <glm/mat3x3.hpp>
+#include <glm/vec3.hpp>
+#include <glm/gtc/quaternion.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/transform.hpp>
+#include <glm/gtx/matrix_decompose.hpp>
+
+#include <QApplication>
+#include <QWidget>
+#include <QDebug>
+#include <QVariant>
+#include <QMainWindow>
+#include <QTimer>
+#include <QSplitter>
+#include <QToolBar>
+#include <QMessageBox>
+#include <QOpenGLWidget>
+#include <QOpenGLContext>
+#include <QMouseEvent>
+#include <QWheelEvent>
 
 template<typename T>
 using Unique_Ptr = std::unique_ptr<T>;
@@ -19,33 +48,8 @@ using Fun = std::function<T>;
 template<typename T>
 using Optional = std::optional<T>;
 
-#define GLM_ENABLE_EXPERIMENTAL
-
-#include <cassert>
-#include <cstdio>
-#include <vector>
-#include <unordered_map>
-#include <glm/gtc/constants.hpp>
-#include <glm/mat3x3.hpp>
-#include <glm/vec3.hpp>
-#include <glm/gtc/quaternion.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtx/transform.hpp>
-#include <glm/gtx/matrix_decompose.hpp>
-
-#include <KHR/khrplatform.h>
-#include <glad/glad.h>
-
 using Vec2 = glm::vec2;
 using Vec3 = glm::vec3;
 using Mat3 = glm::mat3;
 using Mat4 = glm::mat4;
 using Quat = glm::quat;
-#else
-
-#include <assert.h>
-#include <stdio.h>
-#include <KHR/khrplatform.h>
-#include <glad/glad.h>
-
-#endif /* __cplusplus */
