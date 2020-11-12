58b8adac13d30ee67b0c6dc53fa60b8f11b8ad80
Daniel Meszaros <easimer@gmail.com>
2020-11-12 03:37:32 +0100

    fix benchmark_load_test.sh

--- tools/benchmark_load_test.sh
+++ tools/benchmark_load_test.sh
@@ -10,7 +10,7 @@ RUN_ID=`date +%s`
 DATA_DIR="data_$RUN_ID"
 mkdir $DATA_DIR
 
-for BACKEND in `seq 3 3`; do
+for BACKEND in `seq 1 3`; do
     echo "==========================="
     echo "Benching backend #$BACKEND"
     for SIZE in `seq 4 115`; do
