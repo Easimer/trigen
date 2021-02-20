1f1fda194cbeae94fa72f339c6fc882b4ef2c3ed
Daniel Meszaros <easimer@gmail.com>
2021-02-20 21:26:52 +0100

    mesh_paint_proto: add virtual dtor to ISession

--- mesh_paint_proto/session.h
+++ mesh_paint_proto/session.h
@@ -10,6 +10,8 @@
 
 class ISession {
 public:
+    virtual ~ISession() = default;
+
     virtual void render(gfx::Render_Queue *rq) = 0;
 
     virtual marching_cubes::params &marching_cubes_params() = 0;
