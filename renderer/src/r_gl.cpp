875a2c8e61c9a679d9a75800770152599a997024
Daniel Meszaros <easimer@gmail.com>
2021-06-01 09:00:51 +0200

    renderer: move Array_Recycler<T> to gl_utils.h

--- renderer/src/r_gl.cpp
+++ renderer/src/r_gl.cpp
@@ -21,9 +21,10 @@
 #include <array>
 #include <list>
 
+#include "gl_utils.h"
 #include "r_gl_shadercompiler.h"
-#include "shader_program_builder.h"
 #include "shader_generic.h"
+#include "shader_program_builder.h"
 
 #include <Tracy.hpp>
 
@@ -87,68 +88,6 @@ struct Point {
 struct Element_Model {
     gl::VAO arr;
     gl::VBO vertices, elements, colors;
-};
-
-/**
- * VAO and VBO recycler for stream drawing.
- */
-template<typename Tuple>
-struct Array_Recycler {
-public:
-    /**
-     * Get an unused instance of `Tuple`.
-     * @param out Where the pointer to the tuple will be placed.
-     * @return Handle to the instance.
-     */
-    size_t get(Tuple** out) {
-        *out = NULL;
-        if (ready_queue.empty()) {
-            return make_new(out);
-        } else {
-            auto ret = ready_queue.front();
-            ready_queue.pop();
-            *out = &arrays[ret];
-            return ret;
-        }
-    }
-
-    /**
-     * Mark a tuple instance as used and retire it.
-     * @param handle An instance handle returned from get(Tuple**).
-     */
-    void put_back(size_t handle) {
-        assert(handle < arrays.size());
-        retired_queue.push(handle);
-    }
-
-    /**
-     * Called after a frame ends.
-     */
-    void flip() {
-        while (!retired_queue.empty()) {
-            auto h = retired_queue.front();
-            retired_queue.pop();
-            ready_queue.push(h);
-        }
-
-        assert(retired_queue.size() == 0);
-        assert(ready_queue.size() == arrays.size());
-    }
-
-    long long count() const {
-        return retired_queue.size();
-    }
-protected:
-    size_t make_new(Tuple** out) {
-        auto ret = arrays.size();
-        arrays.push_back(Tuple());
-        *out = &arrays.back();
-        return ret;
-    }
-private:
-    std::queue<size_t> ready_queue;
-    std::queue<size_t> retired_queue;
-    std::vector<Tuple> arrays;
 };
 
 class GL_Renderer : public gfx::IRenderer {
