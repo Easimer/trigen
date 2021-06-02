9ba34cb411b4759c45faaa26e847021751e1adc9
Daniel Meszaros <easimer@gmail.com>
2021-06-02 08:45:28 +0200

    virtfs: fix static initialization order bug

--- libs/virtfs/src/virtfs.cpp
+++ libs/virtfs/src/virtfs.cpp
@@ -15,12 +15,22 @@ struct Entry {
     std::optional<std::string> onDisk;
 };
 
-static std::unordered_map<std::string, Entry> files;
+using Entry_Map = std::unordered_map<std::string, Entry>;
+static Entry_Map *files = nullptr;
 
 namespace virtfs {
 
+static void Initialize() {
+    if(files != nullptr) return;
+    files = new Entry_Map;
+}
+
 Status GetFile(std::string const &path, char const **contents) {
-    if (files.count(path) == 0) {
+    if(files == nullptr) {
+        Initialize();
+    }
+
+    if (files->count(path) == 0) {
         return Status::NotFound;
     }
 
@@ -28,7 +38,7 @@ Status GetFile(std::string const &path, char const **contents) {
         return Status::InvalidArguments;
     }
 
-    auto &entry = files.at(path);
+    auto &entry = files->at(path);
 
     if (entry.onDisk) {
         *contents = entry.onDisk->c_str();
@@ -40,11 +50,15 @@ Status GetFile(std::string const &path, char const **contents) {
 }
 
 Status RegisterFile(std::string const &path, char const *contents) {
+    if(files == nullptr) {
+        Initialize();
+    }
+
     if (contents == nullptr || path.length() == 0) {
         return Status::InvalidArguments;
     }
 
-    if (files.count(path) != 0) {
+    if (files->count(path) != 0) {
         return Status::Duplicate;
     }
 
@@ -60,7 +74,7 @@ Status RegisterFile(std::string const &path, char const *contents) {
         printf("[ virtfs ] file '%s' found on disk, replacing the in-memory version\n", path.c_str());
     }
 
-    files[path] = std::move(entry);
+    (*files)[path] = std::move(entry);
 
     return Status::Ok;
 }
