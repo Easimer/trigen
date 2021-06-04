e0ac9e0aae1c6009edd0f1c6cfffa58f508bdb49
Daniel Meszaros <easimer@gmail.com>
2021-06-04 17:40:06 +0200

    trigen: add c_compile_test.c

---
+++ libs/trigen/src/c_compile_test.c
@@ -0,0 +1,9 @@
+// === Copyright (c) 2020-2021 easimer.net. All rights reserved. ===
+//
+// Purpose: this file includes trigen.h to check whether it's a valid C header
+// file
+//
+
+#include <trigen.h>
+
+static void dummy() {}
