48dac68f0f2bd5bd85aee164af2668c218256a29
Daniel Meszaros <easimer@gmail.com>
2021-06-04 15:22:46 +0200

    psp: deprecate old API

--- libs/psp/include/psp/psp.h
+++ libs/psp/include/psp/psp.h
@@ -24,12 +24,12 @@ namespace PSP {
         std::vector<glm::u8vec3> chart_debug_color;
     };
 
-    struct Texture {
+    struct [[deprecated]] Texture {
         int width, height;
         void const *buffer;
     };
 
-    struct Material {
+    struct [[deprecated]] Material {
         Texture base;
         Texture normal;
         Texture height;
@@ -37,7 +37,7 @@ namespace PSP {
         Texture ao;
     };
 
-    class IPainter {
+    class [[deprecated]] IPainter {
     public:
         virtual ~IPainter() = default;
 
@@ -52,7 +52,7 @@ namespace PSP {
 
     int unwrap(/* inout */ Mesh &mesh);
 
-    struct Parameters {
+    struct [[deprecated]] Parameters {
         PSP::Mesh const *mesh;
         PSP::Material const *material;
 
@@ -66,29 +66,48 @@ namespace PSP {
     [[deprecated]]
     std::unique_ptr<PSP::IPainter> make_painter(Parameters const &params);
 
+    // ==========================
     // NEW API
+    // ==========================
 
+    /** \brief An input texture descriptor. Doesn't own the buffer. */ 
     struct Input_Texture {
         unsigned width, height;
         void const *buffer;
     };
 
+    /** \brief A list of input textures. */
     using Input_Material = std::vector<Input_Texture>;
 
+    /** \brief An output textures. */
     struct Output_Texture {
         unsigned width, height;
         std::unique_ptr<uint8_t[]> buffer;
     };
 
+    /** \brief A list of output textures. */
     using Output_Material = std::vector<Output_Texture>;
 
+    /** \brief Painting parameters. */
     struct Paint_Input {
+        /** Pointer to the mesh; can't be NULL. */
         PSP::Mesh const *mesh;
+        /** Reference to the input material */
         Input_Material const &inputMaterial;
 
+        /** Width of the output textures. Can't be zero. */
         unsigned outWidth;
+        /** Height of the output textures. Can't be zero. */
         unsigned outHeight;
     };
 
+    /**
+     * Performs texture painting using the input textures provided.
+     * Takes a mesh, a list of input materials and the desired output size and returns
+     * a material (with the same number of textures as the input material).
+     *
+     * \param params Parameters
+     * \return The output material.
+     */
     Output_Material paint(Paint_Input const &params);
 }
