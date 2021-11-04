f244e7fa3e6a98ab8d95804cb40ea3547cc0bd2c
Daniel Meszaros <easimer@gmail.com>
2021-11-04 10:28:42 +0100

    testbed_qt: bugfixes

--- testbed_qt/main.cpp
+++ testbed_qt/main.cpp
@@ -6,12 +6,16 @@
 #include "common.h"
 #include "wnd_main.h"
 #include <QApplication>
+#include <imgui.h>
 
 int main(int argc, char** argv) {
     QSurfaceFormat fmt;
     fmt.setVersion(4, 4);
     fmt.setProfile(QSurfaceFormat::CoreProfile);
     QSurfaceFormat::setDefaultFormat(fmt);
+
+    auto *ctx = ImGui::CreateContext();
+    ImGui::SetCurrentContext(ctx);
 
     QApplication app(argc, argv);
 
